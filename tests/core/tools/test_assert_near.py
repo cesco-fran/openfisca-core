01a899a002ec4c1d964a38fbe0e49c2d4a7cc06a
cescofran <francesco>
2021-01-19 22:38:43 +0100

    Separate test_countries fixtures and make them accessible to others tests

--- tests/core/tools/test_assert_near.py
+++ tests/core/tools/test_assert_near.py
@@ -3,17 +3,15 @@ import numpy as np
 
 from openfisca_core.tools import assert_near
 
-from ..test_countries import tax_benefit_system
 
-
-def test_enum():
+def test_enum(tax_benefit_system):
     possible_values = tax_benefit_system.variables['housing_occupancy_status'].possible_values
     value = possible_values.encode(np.array(['tenant']))
     expected_value = 'tenant'
     assert_near(value, expected_value)
 
 
-def test_enum_2():
+def test_enum_2(tax_benefit_system):
     possible_values = tax_benefit_system.variables['housing_occupancy_status'].possible_values
     value = possible_values.encode(np.array(['tenant', 'owner']))
     expected_value = ['tenant', 'owner']
